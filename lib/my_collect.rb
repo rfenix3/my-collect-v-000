90af31f23be0b9133465613b480bb0cc79d743de
Richard Fenix <richard.fenix@gmail.com>
2018-05-30 02:52:19 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+def my_collect(array)
+  i = 0
+  collection = []
+  while i < array.length do 
+    collection << yield(array[i])
+    i = i + 1
+  end
+  collection
+end
 
+#puts my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]) {|name| name.split(" ").first}.inspect
 
+#puts my_collect(["ruby", "javascript", "python"]) {|lang| lang.upcase}.inspect
