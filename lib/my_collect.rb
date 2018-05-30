5915c92a1dc300cb3c82c17a7f7cb146ceccaa44
Richard Fenix <richard.fenix@gmail.com>
2018-05-30 02:51:27 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -8,6 +8,6 @@ def my_collect(array)
   collection
 end
 
-puts my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]) {|name| name.split(" ").first}.inspect
+#puts my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]) {|name| name.split(" ").first}.inspect
 
-puts my_collect(["ruby", "javascript", "python"]) {|lang| lang.upcase}.inspect
+#puts my_collect(["ruby", "javascript", "python"]) {|lang| lang.upcase}.inspect
