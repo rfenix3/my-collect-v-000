97e0d930a9d73f2c02d5c3cc3967592172506c2b
Richard Fenix <richard.fenix@gmail.com>
2018-05-30 02:45:12 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(array)
   i = 0
   collection = []
   while i < array.lenght do 
-    collection << yield (element[i])
+    collection << yield (array[i])
     i = i + 1
   end
   collection
